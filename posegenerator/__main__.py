3571c8fd70c82b1ee6e610271403aa94e772226d
coyoung <coyoung@umich.edu>
2022-03-06 22:14:27 -0500

    Improved comments

--- posegenerator/__main__.py
+++ posegenerator/__main__.py
@@ -14,6 +14,8 @@ import numpy as np
               help="Print more output.")
 def main(input_video, poses_json, output_video, verbose):
 
+    # Default values for body parts, connections, and color.
+    # TODO: Read in these parameters from a JSON file
     bodyparts = ["head", "lankle", "lelbow", "lhip", "lknee", "lshoulder", "lwrist",
                  "pelvis", "rankle", "relbow", "rhip", "rknee", "rshoulder", "rwrist",
                  "thorax", "upperneck"]
@@ -43,7 +45,7 @@ def main(input_video, poses_json, output_video, verbose):
     colors.fill(defaultColor)
 
 
-    # Load in the poses json file
+    # Load in the poses JSON file
     try:
         with open(str(poses_json),
                   "r", encoding="utf-8") as poses:
@@ -55,16 +57,18 @@ def main(input_video, poses_json, output_video, verbose):
         print("Error reading pose JSON. Is it properly formatted?")
         sys.exit(1)
 
+    # Read in the input video
     input = cv2.VideoCapture(input_video)
 
-    fps = int(input.get(2))
+    # Get relevant metadata about video
     frame_width = int(input.get(3))
     frame_height = int(input.get(4))
-
     fps = input.get(cv2.CAP_PROP_FPS)
 
+    # Prepare output video for writing
     output = cv2.VideoWriter(output_video, cv2.VideoWriter_fourcc('m','p','4','v'), fps, (frame_width,frame_height))
 
+    # Check for valid input/output
     if (input.isOpened()== False):
         print("Error opening input video.")
         sys.exit(1)
@@ -73,26 +77,27 @@ def main(input_video, poses_json, output_video, verbose):
         print("Error opening output video.")
         sys.exit(1)
 
+    # Used to track current frame and progress
     frameCount = 0
-
     totalFrames = int(input.get(cv2.CAP_PROP_FRAME_COUNT))
 
+    # Loop through every frame of the input vide
     while(input.isOpened()):
         ret, frame = input.read()
+        # If a valid frame
         if ret == True:
-
             if frameCount <= totalFrames and verbose:
                 print("Processing frame " + str(1+frameCount) + " of " + str(totalFrames), end="")
 
+            # Capture coordinates of each body part from JSON file for current frame
             for i in range(len(bodyparts)):
                 x = int(pose_data[frameCount][bodyparts[i]]["coords"][0])
                 y = int(pose_data[frameCount][bodyparts[i]]["coords"][1])
-
                 coords[i] = (x,y)
 
             if verbose: print(".",end="")
 
-            # Connect joints
+            # Connect joints according to connections array
             for i in range(len(connections)):
                 x1 = coords[connections[i][0]][0]
                 y1 = coords[connections[i][0]][1]
@@ -108,11 +113,13 @@ def main(input_video, poses_json, output_video, verbose):
             for i in range(len(coords)):
                 cv2.circle(frame, (coords[i][0], coords[i][1]), 5, (201, 91, 0), -1)
 
+            # Write output frame
             output.write(frame)
 
             if verbose: print(".done")
 
         else:
+            # Break loop when done with the input video
             break
         frameCount += 1
 
